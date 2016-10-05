5917c30f82c7f4849571e2303c0ee8d4a5d5a3c5
Chris <chk.mue@icloud.com>
2016-10-05 17:53:11 +0200

    1st try for building an AppImage w/ linuxdeployqt

--- ci/ci-build-linux.sh
+++ ci/ci-build-linux.sh
@@ -6,3 +6,15 @@ echo "Inside build-linux.sh"
 qmake -project
 qmake CmWidget1.pro
 make
+
+# Now try to build an AppImage
+pwd
+wget https://github.com/probonopd/linuxdeployqt/releases/download/1/linuxdeployqt-1-x86_64.AppImage -O linuxdeployqt.AppImage
+ls -la
+chmod +x linuxdeployqt.AppImage
+
+export LD_LIBRARY_PATH=/usr/local/lib
+./linuxdeployqt CmWidget1 -appimage
+
+ls -la
+du -sch CmWidget1.AppImage
