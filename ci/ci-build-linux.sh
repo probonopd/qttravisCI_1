cdf161d1d98f2b8ea6dff7e8daf2bf1d4520eae0
probonopd <probonopd@users.noreply.github.com>
2016-10-05 21:26:16 +0200

    Update ci-build-linux.sh

--- ci/ci-build-linux.sh
+++ ci/ci-build-linux.sh
@@ -15,4 +15,8 @@ cp CmWidget1 AppDir/
 wget https://github.com/probonopd/linuxdeployqt/releases/download/1/linuxdeployqt-1-x86_64.AppImage -O linuxdeployqt
 chmod +x linuxdeployqt
 
+# Work around a bug in linuxdeployqt not being able to bundle system Qt's plugins
+sudo ln -s /usr/lib/x86_64-linux-gnu/qt5/plugins /usr/lib/plugins
+
+# Have linuxdeployqt create the AppImage
 ./linuxdeployqt CmWidget1 -appimage -bundle-non-qt-libs -verbose=3
