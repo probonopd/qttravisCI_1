eccdbacd5a863929c0fea8054c53a6dad812a0dd
probonopd <probonopd@users.noreply.github.com>
2016-10-05 21:06:05 +0200

    Update ci-build-linux.sh

--- ci/ci-build-linux.sh
+++ ci/ci-build-linux.sh
@@ -10,7 +10,8 @@ strip CmWidget1
 ldd CmWidget1
 
 # Now try to build an AppImage
-mkdir AppDir ; cpCmWidget1  AppDir/
+mkdir AppDir
+cp cpCmWidget1  AppDir/
 wget https://github.com/probonopd/linuxdeployqt/releases/download/1/linuxdeployqt-1-x86_64.AppImage -O linuxdeployqt
 chmod +x linuxdeployqt
 
