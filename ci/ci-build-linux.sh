c9920cd18e527aefd8fa118fc879f3eae8383a06
probonopd <probonopd@users.noreply.github.com>
2016-10-05 20:59:00 +0200

    Update ci-build-linux.sh

--- ci/ci-build-linux.sh
+++ ci/ci-build-linux.sh
@@ -6,15 +6,15 @@ echo "Inside build-linux.sh"
 qmake -project
 qmake CmWidget1.pro
 make
+strip CmWidget1
+ldd CmWidget1
 
 # Now try to build an AppImage
-pwd
-wget https://github.com/probonopd/linuxdeployqt/releases/download/1/linuxdeployqt-1-x86_64.AppImage -O linuxdeployqt.AppImage
-ls -la
-chmod +x linuxdeployqt.AppImage
-ldd CmWidget1
-export LD_LIBRARY_PATH="/usr/lib/x86_64-linux-gnu":$LD_LIBRARY_PATH
-./linuxdeployqt.AppImage CmWidget1 -appimage -bundle-non-qt-libs -verbose=3
+mkdir AppDir ; cp AppDir/CmWidget1
+wget https://github.com/probonopd/linuxdeployqt/releases/download/1/linuxdeployqt-1-x86_64.AppImage -O linuxdeployqt
+chmod +x linuxdeployqt
+
+./linuxdeployqt AppDir/CmWidget1 -appimage -bundle-non-qt-libs -verbose=3
 
 ls -la
 mv ..AppImage CmWidget1.AppImage
