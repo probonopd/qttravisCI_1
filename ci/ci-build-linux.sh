d4b40bee3e20903441c4fed8e248b797bc7fb5db
probonopd <probonopd@users.noreply.github.com>
2016-10-05 21:11:07 +0200

    Update ci-build-linux.sh

--- ci/ci-build-linux.sh
+++ ci/ci-build-linux.sh
@@ -15,8 +15,4 @@ cp cpCmWidget1  AppDir/
 wget https://github.com/probonopd/linuxdeployqt/releases/download/1/linuxdeployqt-1-x86_64.AppImage -O linuxdeployqt
 chmod +x linuxdeployqt
 
-./linuxdeployqt AppDir/CmWidget1 -appimage -bundle-non-qt-libs -verbose=3
-
-ls -la
-mv ..AppImage CmWidget1.AppImage
-du -sch CmWidget1.AppImage
+./linuxdeployqt $(realpath AppDir/CmWidget1) -appimage -bundle-non-qt-libs -verbose=3
